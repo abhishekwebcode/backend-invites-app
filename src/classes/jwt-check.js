db40ccb7dd78af65e3395ef8e5a8694a51137c18
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-04-23 19:20:08 +0530

    y

--- src/classes/jwt-check.js
+++ src/classes/jwt-check.js
@@ -1,11 +1,10 @@
 module.exports=function (app) {
-    //TODO Bypassed JWT AUTH
-    return;
     const bearerToken = require('express-bearer-token');
     app.use(bearerToken());
     app.all("*",async function (req, res, next) {
         //let meta = require(`../auth/jwt/verify`)(res.token, res.email, res);
         let meta = await (require("../auth/jwt/jwt").getPayloadFromToken(req.token));
+        console.dir(meta);
         if (!meta) { res.end() ; return ; }
         req.User = meta;
         req.email = meta.email;
