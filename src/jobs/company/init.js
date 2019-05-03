98e992af7356876cd175a33fbffadcd180607ff2
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-03 20:38:07 +0530

    top companies flutter integrate

--- src/jobs/company/init.js
+++ src/jobs/company/init.js
@@ -1,5 +1,5 @@
 module.exports=function (app) {
-    app.all("/companies/Top",async function(req,res,next) {
+    app.all("/jobs/companies/top",async function(req,res,next) {
         let topCompanies = await require("./getTopCompanies")(req.app.get("db"),req.fields.offset||0,req.fields.limit||10);
         res.json({
             success:true,
