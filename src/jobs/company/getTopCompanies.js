fb44f100e05ae14ec8cc3ce3fd8e91a2ff49e87a
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-03 11:03:03 +0530

    coreected TOp Comapnies API

--- src/jobs/company/getTopCompanies.js
+++ src/jobs/company/getTopCompanies.js
@@ -1,6 +1,6 @@
 
 module.exports=async function(DB,offset,limit,filter) {
-    let companies = await DB().collection(`companies`).find({},{id:1,title:1,logo_url:1,jobs:1}).sort({jobs:-1}).skip(offset).limit(limit);
+    let companies = await DB().collection(`companies`).find({},{id:1,title:1,logo_url:1,jobs:1}).sort({jobs:-1}).skip(offset).limit(limit).toArray();
     if (filter) companies=filter(companies);
     return companies;
 };
