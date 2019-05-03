10bd128ee12c50c2725f386b1bbb0f4088294b00
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-03 11:01:32 +0530

    coreected TOp Comapnies API

--- src/jobs/company/init.js
+++ src/jobs/company/init.js
@@ -1,6 +1,6 @@
 module.exports=function (app) {
     app.all("/companies/Top",async function(req,res,next) {
-        let topCompanies = await require("./getTopCompanies")(req.app.get("db"),req.fields.offset,req.fields.limit||10);
+        let topCompanies = await require("./getTopCompanies")(req.app.get("db"),req.fields.offset||0,req.fields.limit||10);
         res.json({
             success:true,
             result:topCompanies
