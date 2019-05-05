ace7f3b5234b9bb042452b4fdeb38d543cc1b4fe
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-05 20:17:42 +0530

    Added show jobs mock api for myjobs UI development

--- src/jobs/ShowJobs.js
+++ src/jobs/ShowJobs.js
@@ -1,12 +1,13 @@
 async function getJobsList(DB) {
     //TODO IMPLEMENT JOB LISTING LOGIC
     let mock = {
+        id:`kdsjif43oi5j34i5nh3hu5`,
         title:`Event Team Leader`,
         salary:`RM2,000.00-RM2,500.00 Per Month`,
         type:`Full Time`,
         company_url:`https://source.unsplash.com/random/100x100`,
         tag:`Urgent Job!`,
-        company_bane:`TC Advertising Sdn Bhd (Orins Advertising)`
+        company_name:`TC Advertising Sdn Bhd (Orins Advertising)`
     };
     let r=[];
     for (let i = 0; i < 10 ; i++) {
