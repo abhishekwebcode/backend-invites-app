3787bad0ab602199587d5ae621cf869e821a1b45
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-12 19:57:51 +0530

    corrected projection

--- src/main/start.js
+++ src/main/start.js
@@ -44,6 +44,9 @@ app.all('/google_auth', user_auth.google_auth);
 app.all('/facebook_auth', user_auth.facebook);
 // require auth to proceed
 require(`../classes/jwt-check`)(app);
+
+//ALl other session related funcations below
+
 //enable self identity functions
 require(`../me/init`)(app);
 // tests module
--- src/me/init.js
+++ src/me/init.js
@@ -2,10 +2,10 @@ module.exports=function (app) {
     app.all(`/me`,async function (request,response) {
         let email = request.email;
         console.log(email);
-        let res=await request.app.get("db")().collection(`users`).find({email}).limit(1).toArray();
+        let res=await request.app.get("db")().collection(`users`).find({email},{email:1,username:1,_id:0}).limit(1).toArray();
         response.json({
             success:true,
-            data:res
+            data:(res.length==0)?[{email:"Not found",username:"..."}]:res[0]
         })
     });
 }
