30c0097134c1247dccf99fa125734d0f800035d7
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-12 19:14:21 +0530

    corrected sign up

--- src/jobs/company/init.js
+++ src/jobs/company/init.js
@@ -1,6 +1,6 @@
 module.exports=function (app) {
     app.all("/jobs/companies/top",async function(req,res,next) {
-        let topCompanies = await require("./getTopCompanies")(req.app.get("db"),req.fields.offset||0,req.fields.limit||10);
+        let topCompanies = await require("./getTopCompanies")(req.app.get("db"),req.fields.offset||0,req.fields.limit||100);
         res.json({
             success:true,
             result:topCompanies
