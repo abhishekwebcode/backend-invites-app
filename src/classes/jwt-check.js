d33a663ec0c8201b4255253358e9e15d8629ec88
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-12 19:45:25 +0530

    debug signup

--- src/classes/jwt-check.js
+++ src/classes/jwt-check.js
@@ -3,6 +3,7 @@ module.exports=function (app) {
     app.use(bearerToken());
     app.all("*",async function (req, res, next) {
         //let meta = require(`../auth/jwt/verify`)(res.token, res.email, res);
+        console.log("token is",req.token);
         let meta = await (require("../auth/jwt/jwt").getPayloadFromToken(req.token));
         console.dir(meta);
         if (!meta) { res.json({success:false,message:"NOT_LOGGED_IN"}); res.end() ; return ; }
