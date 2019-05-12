a63dafbfcd9eab714a0b239d407fef0d1cfc88a9
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-12 19:35:37 +0530

    added me enpoint for home

---
+++ src/me/init.js
@@ -0,0 +1,10 @@
+module.exports=function (app) {
+    app.all(`/me`,async function (request,response) {
+        let email = request.email;
+        let res=await request.app.get("db")().collection(`users`).find({email}).limit(1).toArray();
+        response.json({
+            success:true,
+            data:res
+        })
+    });
+}
