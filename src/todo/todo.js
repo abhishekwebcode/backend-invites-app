c9f8c60be0ebde5950592f40719690b8e74a34cc
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-19 21:09:32 +0530

    added todo functions corrected

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,5 +1,5 @@
 module.exports=function (app) {
-    app.post(`/todo/list`,async function (request,response) {
+    app.post(`/todos/list`,async function (request,response) {
         let todos = await app.get(`db`)().collection(`todo`).find({
             created_by:request.email,eventId:request.fields.eventId
         }).project({_id:1}).sort({}).skip(parseInt(request.fields.offset)).limit(10).toArray();
@@ -7,10 +7,12 @@ module.exports=function (app) {
             success:true,todos
         })
     });
-    app.post(`/todo/create`,async function (request,response) {
+    app.post(`/todos/create`,async function (request,response) {
         let todo = request.fields.todo;
         let eventId = request.fields.eventId;
-        let todoIns = await app.get(`db`)().collection(`events`).insertOne({todo,eventId,created_by:request.email,date_created:Date.now()});
+        let todoIns = await app.get(`db`)().collection(`events`).insertOne({
+            todo,eventId,created_by:request.email,date_created:Date.now(),done:false
+        });
         if (todoIns.insertedCount==1) {response.json({success: true})}
         else {response.json({success: false,message:`Error creating your task`});}
         return ;
