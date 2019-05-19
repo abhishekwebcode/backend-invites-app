3fff70984c97355aa76cb5e0c847131cbc3b0725
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-20 00:53:10 +0530

    coorected from crashd

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -11,7 +11,7 @@ module.exports=function (app) {
     app.post(`/todos/update`,async function (request,response) {
         let todo = request.fields.item;
         let status = request.fields.status;
-        console.dir((
+        console.log((
             {
                 _id:app.get(`id`)(todo)
             },{
