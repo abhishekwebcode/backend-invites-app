8514b755b329024dc03c073f9c5786ec708d3d40
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-19 18:57:43 +0530

    added event adder coorected schema

--- src/events/event.js
+++ src/events/event.js
@@ -9,11 +9,19 @@ module.exports=function (app) {
     app.post(`/events/add`,async function(request,response) {
         console.log(arguments);
         let event=JSON.parse(request.fields.event);
+        event["date"]=new Date(parseInt(event["date"]));
+        event["isSpecialTheme"]=(event["isSpecialTheme"]=="true"?true:false);
+        event["guestSee"]=(event["guestSee"]=="true"?true:false);
         console.log(event);
         let events = await app.get(`db`)().collection(`events`).insertOne(
             {...event,created_by:request.email,date_created:Date.now()}
         );
-        console.log(events);
-        response.json({success: true})
+        if (events.insertedCount==1) {
+            response.json({success: true})
+        }
+        else
+            {response.json({success: false,message:`Error creating your party`})}
+        return ;
     });
+
 };
