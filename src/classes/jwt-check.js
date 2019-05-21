07addd316ddd2604771847de733aa9b668818aef
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-21 22:13:07 +0530

    error handling

--- src/classes/jwt-check.js
+++ src/classes/jwt-check.js
@@ -1,14 +1,22 @@
+const bearerToken = require('express-bearer-token');
 module.exports=function (app) {
-    const bearerToken = require('express-bearer-token');
     app.use(bearerToken());
-    app.all("*",async function (req, res, next) {
-        //let meta = require(`../auth/jwt/verify`)(res.token, res.email, res);
-        console.log("token is",req.token);
-        let meta = await (require("../auth/jwt/jwt").getPayloadFromToken(req.token));
-        console.dir(meta);
-        if (!meta) { res.json({success:false,message:"NOT_LOGGED_IN"}); res.end() ; return ; }
-        req.User = meta;
-        req.email = meta.email;
-        next();
+    app.all("*", async function (req, res, next) {
+        try {
+            //let meta = require(`../auth/jwt/verify`)(res.token, res.email, res);
+            console.log("token is", req.token);
+            let meta = await (require("../auth/jwt/jwt").getPayloadFromToken(req.token));
+            console.dir(meta);
+            if (!meta) {
+                res.json({success: false, message: "NOT_LOGGED_IN"});
+                res.end();
+                return;
+            }
+            req.User = meta;
+            req.email = meta.email;
+            next();
+        } catch (e) {
+         res.json({success:false,message:`Your login has expired,please login again...`})
+        }
     });
 };
