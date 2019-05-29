4be51e86f22b1fad83c5666e0dd2e741d145c115
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-05-29 15:06:52 +0530

    invites info

--- src/events/invites.js
+++ src/events/invites.js
@@ -1,59 +1,63 @@
-module.exports=function (app) {
-    app.post(`/invites/list`,async function (request,response) {
+module.exports = function (app) {
+    app.post(`/invites/list`, async function (request, response) {
         console.dir(arguments);
         let db = request.app.get(`db`)();
         console.dir(`inInviesliSt`);
         console.dir(db);
-        let id = await db.collection(`users`).find({email:request.User.email}).limit(1).toArray();
-        let _id= id[0]._id;
-        let userID=request.app.get(`id`)(_id);
-        let invites =await db.collection(`events`).find({
-            users:{
-                $in:[
+        let id = await db.collection(`users`).find({email: request.User.email}).limit(1).toArray();
+        let _id = id[0]._id;
+        let userID = request.app.get(`id`)(_id);
+        let invites = await db.collection(`events`).find({
+            users: {
+                $in: [
                     userID
                 ]
             }
         }).project({
-           _id:1,childName:1,date:1
-        }).sort({date:-1}).skip(parseInt(request.fields.offset)).limit(10).toArray();
+            _id: 1, childName: 1, date: 1
+        }).sort({date: -1}).skip(parseInt(request.fields.offset)).limit(10).toArray();
         response.json({
-            success:true,invites
+            success: true, invites
         })
     });
-    app.post(`/invites/info`,async function (request,response) {
-        let eventID=request.fields.eventId;
+    app.post(`/invites/info`, async function (request, response) {
+        let eventID = request.fields.eventId;
         let eventIDOBJECT = request.app.get(`id`)(eventID);
         console.log(arguments);
         let eventINFO = await request.app.get(`db`)().collection(`events`).find({
-            _id:eventIDOBJECT
+            _id: eventIDOBJECT
         }).project({
-            timeEnd:1,
-            zipCode:1,
-            country:1,
-            isSpecialTheme:1,
-            city:1,
-            timeStart:1,
-            date:1,
-            street:1,
-            childName:1,
-            district:1,
-            otherAddress:1,
-            theme:1,
-            created_by:1
+            timeEnd: 1,
+            zipCode: 1,
+            country: 1,
+            isSpecialTheme: 1,
+            city: 1,
+            timeStart: 1,
+            date: 1,
+            street: 1,
+            childName: 1,
+            district: 1,
+            otherAddress: 1,
+            theme: 1,
+            created_by: 1
         }).toArray();
-        if (eventINFO.length===0) {
-            response.json({success:false,message:"Invite does not exist"});
-            return ;
-        }
-        else {
+        if (eventINFO.length === 0) {
+            response.json({success: false, message: "Invite does not exist"});
+            return;
+        } else {
             let email = eventINFO[0].created_by;
-            let userInfo = await request.app.get(`db`)().collection(`users`).find({email:email}).toArray();
-            response.json({
-                success:true,
-                invite:eventINFO[0],
-                owner:userInfo[0]
+            let userInfo = await request.app.get(`db`)().collection(`users`).find({email: email}).toArray();
+            console.dir({
+                success: true,
+                invite: eventINFO[0],
+                owner: userInfo[0]
             });
-            return ;
+            response.json({
+                success: true,
+                invite: eventINFO[0],
+                owner: userInfo[0]
+            });
+            return;
         }
     });
 };
