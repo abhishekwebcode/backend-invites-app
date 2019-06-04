9030128adca68f1a964d5058918f0c21bc4786a3
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-06-04 16:05:31 +0530

    added gifts list for invitees

--- src/gifts/gifts.js
+++ src/gifts/gifts.js
@@ -1,7 +1,8 @@
 module.exports=function (app) {
     app.post(`/gifts/list`,async function (request,response) {
+        let eventId = request.app.get(`id`)(request.fields.eventId);
         let gifts = await app.get(`db`)().collection(`gifts`).find({
-            created_by:request.email,eventId:request.fields.eventId
+            created_by:request.email,eventId
         }).project({_id:1,gift:1,selected:1,date_created:1}).sort({gift:1}).skip(parseInt(request.fields.offset)).limit(10).toArray();
         response.json({
             success:true,gifts
@@ -20,7 +21,7 @@ module.exports=function (app) {
     })
     app.post(`/gifts/add`,async function (request,response) {
         let gift = request.fields.todo;
-        let eventId = request.fields.eventId;
+        let eventId = request.app.get(`id`)(request.fields.eventId);
         let todoIns = await app.get(`db`)().collection(`gifts`).insertOne({
             gift,eventId,created_by:request.email,date_created:Date.now(),selected:false,selected_by_id:null
         });
