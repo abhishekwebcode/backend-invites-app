2da1c4068342538e14fabe71b1820a1c5020a93c
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-06-19 17:07:20 +0530

    me

--- src/me/init.js
+++ src/me/init.js
@@ -2,7 +2,7 @@ module.exports=function (app) {
     app.all(`/me/profile`,async function (request,response) {
         let email = request.email;
         console.log(email);
-        let res=await request.app.get("db")().collection(`users`).find({email},{email:1,username:1}).limit(1).toArray();
+        let res=await request.app.get("db")().collection(`users`).find({email}).project({email:1,username:1}).limit(1).toArray();
         response.json({
             success:true,
             data:(res.length==0)?[{email:"Not found",username:"..."}]:res[0]
