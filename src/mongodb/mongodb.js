f211540c90d8298bd89314571eff68ec658edfb4
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-06-20 12:34:29 +0530

    better mongodb cnnection

--- src/mongodb/mongodb.js
+++ src/mongodb/mongodb.js
@@ -1,28 +1,61 @@
 let options = {
-    server: {
         auto_reconnect: true,
-        socketOptions: {
-            keepAlive: 1,
-            connectTimeoutMS: 6000000,
-            socketTimeoutMS: 6000000,
-        }
-    },
-    replset: {
-        auto_reconnect: true,
-        socketOptions: {
-            keepAlive: 1,
-            connectTimeoutMS: 6000000,
-            socketTimeoutMS: 6000000,
-        }
-    }
+        keepAlive: 1,
+        connectTimeoutMS: 6000000,
+        socketTimeoutMS: 6000000,
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_SAFE_INTEGER,
+    reconnectInterval: 1000,
+    poolSize: 10,
+    bufferMaxEntries: 0
 };
 const uri = "mongodb://localhost:27017";
 const MongoClient = require('mongodb').MongoClient;
 var db;
+function createDBconnection() {
+    MongoClient.connect(uri, options, function (err, client) {
+        if (err) {
+            console.error(err);
+            console.log(`DB FAILED CONNECTION`)
+        };
+        db = client.db(`test`);
+        console.log(`MONGODB CONNECTED NOW`);
+        try {
+            db.on('reconnectFailed', (err) => {
+                console.log(`MONGODB ERROR RECONNECT FAILED`);
+                console.warn(err);
+                createDBconnection()
+            });
+            db.s.topology.on('close', () => {
+                console.log('Connection closed');
+            });
+            db.s.topology.on('reconnect', () => console.log('Reconnected MONGODB'));
+        } catch (e) {
+            console.error(e);
+        }
+    });
+}
+
 MongoClient.connect(uri, options, function (err, client) {
-    if (err) throw err;
+    if (err) {
+        console.error(err);
+        console.log(`DB FAILED CONNECTION`)
+    };
     db = client.db(`test`);
     console.log(`MONGODB CONNECTED NOW`);
+    try {
+        db.on('reconnectFailed', (err) => {
+            console.log(`MONGODB ERROR RECONNECT FAILED`);
+            console.warn(err);
+            createDBconnection()
+        });
+        db.s.topology.on('close', () => {
+            console.log('Connection closed');
+        });
+        db.s.topology.on('reconnect', () => console.log('Reconnected MONGODB'));
+    } catch (e) {
+        console.error(e);
+    }
 });
 /**
  * Returns the sum of a and b
@@ -31,4 +64,5 @@ MongoClient.connect(uri, options, function (err, client) {
 function getDB() {
     return db;
 }
+
 module.exports = getDB;
