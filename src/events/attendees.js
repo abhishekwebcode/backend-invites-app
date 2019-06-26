dc08aa370c0fad55f417512ee0210df7c27e3670
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-06-26 18:16:27 +0530

    fcm integrate

--- src/events/attendees.js
+++ src/events/attendees.js
@@ -3,9 +3,10 @@ module.exports=function (app) {
         let db = request.app.get(`db`)();
         let eventIDObject = request.app.get(`id`)(request.fields.eventId);
         let eventDetails = await db.collection(`events`).find({_id:eventIDObject}).project({users:1,unRegisteredNumbersInternational:1}).toArray();
-        console.dir(eventDetails);
+        //console.dir(eventDetails);
         console.dir(eventIDObject);
         let numbers = eventDetails.unRegisteredNumbersInternational;
+        console.log(eventDetails.users,`array $In`);
         let users = await db.collection(`users`).find({
             _id: { $in : eventDetails.users }
         }).project({name:1,"phone.number":1}).toArray();
