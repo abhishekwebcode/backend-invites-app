ef77f9cb2861355df0a1afc452568ac2158ed3ef
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-08-06 09:57:28 +0530

    names with numbers geetattendees addtition

--- src/events/attendees.js
+++ src/events/attendees.js
@@ -10,7 +10,7 @@ module.exports=function (app) {
     app.post(`/event/getAttendees`,asyncer(async function(request,response,next) {
         let db = request.app.get(`db`)();
         let eventIDObject = request.app.get(`id`)(request.fields.eventId);
-        let eventDetails = await db.collection(`events`).find({_id:eventIDObject}).project({users:1,unRegisteredNumbersInternational:1}).limit(1).toArray();
+        let eventDetails = await db.collection(`events`).find({_id:eventIDObject}).project({users:1,unRegisteredNumbersInternational:1,namesRefined:1}).limit(1).toArray();
         eventDetails=eventDetails[0];
         //console.log(eventDetails);
        //console.log(eventIDObject);
@@ -19,7 +19,7 @@ module.exports=function (app) {
        //console.log(eventDetails.users,`array $In`);
         let users = await db.collection(`users`).find({
             _id: { $in : eventDetails.users }
-        }).project({name:1,"phone.number":1,namesRefined:1}).toArray();
+        }).project({name:1,"phone.number":1}).toArray();
         let usersToSend = [];
         let usersMap = eventDetails.namesRefined;
         let reverse = reverseMap(usersMap);
