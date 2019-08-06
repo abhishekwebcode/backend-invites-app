2c5d14bc8ee49f9c557172384b0384d29d6e5fed
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-08-06 12:46:22 +0530

    add attendees with name

--- src/events/changeContacts.js
+++ src/events/changeContacts.js
@@ -1,34 +1,39 @@
 const PhoneNumber = require('awesome-phonenumber');
+
 //const firebaseAdmin = require(`firebase-admin`);
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
+
 function isPlus(phone) {
     return phone.indexOf(`+`) !== -1;
 }
-function parsePhone(no, intlArray, localArray,prefix) {
+
+function parsePhone(no, intlArray, localArray, prefix) {
     if (isPlus(no)) {
         intlArray.push(new PhoneNumber(no).getNumber());
         return;
     }
-    intlArray.push(prefix+parseInt(no).toString());
+    intlArray.push(prefix + parseInt(no).toString());
 }
+
 function remove(element, array) {
-    if (array.indexOf(element)!==-1) {
+    if (array.indexOf(element) !== -1) {
         array.splice(array.indexOf(element), 1);
     }
     return array;
 }
-async function searchUsers(intlArray,localarray1,  db, emails) {
-   //console.log(db);
-   //console.log(intlArray)
+
+async function searchUsers(intlArray, localarray1, db, emails) {
+    //console.log(db);
+    //console.log(intlArray)
     let attendees = await db.collection(`users`).find({
         $or: [
             {"phone.number": {$in: intlArray}},
             {email: {$in: emails}}
         ]
-    }).project({_id: 1, phone: 1, email: 1,FCM_Tokens:1}).toArray();
-   //console.log(attendees);
+    }).project({_id: 1, phone: 1, email: 1, FCM_Tokens: 1}).toArray();
+    //console.log(attendees);
     let final = [];
     for (i = 0; i < attendees.length; i++) {
         let item = attendees[i];
@@ -45,50 +50,54 @@ async function searchUsers(intlArray,localarray1,  db, emails) {
             final.push(item);
         }
     }
-    return {users: final,localArray:localarray1, emails, intlArray};
+    return {users: final, localArray: localarray1, emails, intlArray};
 }
-async function temPtoken(token,eventIdObject,fcm,sends,OwnerName,childname) {
+
+async function temPtoken(token, eventIdObject, fcm, sends, OwnerName, childname) {
     let message = {
         to: token,
         collapse_key: 'New Invite',
         data: {
-            type:`NEW_INVITE`,
-            eventId:eventIdObject.toString(),
-            Date:Date.now(),
+            type: `NEW_INVITE`,
+            eventId: eventIdObject.toString(),
+            Date: Date.now(),
             OwnerName,
-            Action:`INVITE`,
+            Action: `INVITE`,
             childname: childname
         }
     };
     //console.log(`FOR DEBUG`,fcm,message);
-    let seObj=fcm(message).then(()=>{}).catch(()=>{});
+    let seObj = fcm(message).then(() => {
+    }).catch(() => {
+    });
     sends.push(seObj);
-    return ;
+    return;
     //console.log(seObj)
 }
-var parseIt=function(names,prefix) {
-    let nameRefined={};
+
+var parseIt = function (names, prefix) {
+    let nameRefined = {};
     for (let name in names) {
         let no = names[name];
         let number;
         if (isPlus(no)) {
-            number=(new PhoneNumber(no).getNumber());
+            number = (new PhoneNumber(no).getNumber());
+        } else {
+            number = (prefix + parseInt(no).toString());
         }
-        else {
-            number=(prefix + parseInt(no).toString());
-        }
-        nameRefined[name]=number;
+        nameRefined[name] = number;
     }
     return nameRefined;
 };
-async function sendPush(registeredUsers,ids,db,eventIdObject,app,ownerName,childName) {
-    let allTokens=[];
-    let kjsf=[];
+
+async function sendPush(registeredUsers, ids, db, eventIdObject, app, ownerName, childName) {
+    let allTokens = [];
+    let kjsf = [];
     for (let key in registeredUsers) {
         allTokens.push(...registeredUsers[key]);
     }
     let fcm = app.get(`FCM`);
-   //console.log(`ARGS`,arguments);
+    //console.log(`ARGS`,arguments);
     /*
     registeredUsers.forEach(e=>{
         try {
@@ -98,11 +107,11 @@ async function sendPush(registeredUsers,ids,db,eventIdObject,app,ownerName,child
         }
     });
      */
-   //console.log(allTokens)
-    let sends=[];
-    for (let i = 0; i < allTokens.length ; i++) {
+    //console.log(allTokens)
+    let sends = [];
+    for (let i = 0; i < allTokens.length; i++) {
         let token = allTokens[i];
-        temPtoken(token,eventIdObject,fcm,sends,ownerName,childName).catch(()=>{
+        temPtoken(token, eventIdObject, fcm, sends, ownerName, childName).catch(() => {
 
         });
     }
@@ -116,151 +125,157 @@ async function sendPush(registeredUsers,ids,db,eventIdObject,app,ownerName,child
         },
     )*/
 }
+
 async function sendSMS(nonRegisteredUsers) {
     // NOT REQUIRED AS ITS DONE VIA USER APP
 }
-async  function sendEmails(emails) {
+
+async function sendEmails(emails) {
 
 }
-async function continue_event(numbers, emails1, db,prefix,intlArray1,localArray1) {
+
+async function continue_event(numbers, emails1, db, prefix, intlArray1, localArray1) {
     let {users, localArray, emails, intlArray} = await searchUsers(intlArray1, localArray1, db, emails1);
-   //console.log(users, localArray, emails, intlArray);
+    //console.log(users, localArray, emails, intlArray);
     return {users, localArray, emails, intlArray};
 }
-async function createEvent(numbers, emails1, db,prefix) {
-   //console.log(arguments);
+
+async function createEvent(numbers, emails1, db, prefix) {
+    //console.log(arguments);
     let intlArray1 = [];
     let localArray1 = [];
-    numbers.forEach(e => parsePhone(e, intlArray1, localArray1,prefix));
-    intlArray1=(intlArray1).filter(onlyUnique);
-   //console.log(`intlArray`, intlArray1, `localArray`, localArray1);
-    return {intlArray1,localArray1};
+    numbers.forEach(e => parsePhone(e, intlArray1, localArray1, prefix));
+    intlArray1 = (intlArray1).filter(onlyUnique);
+    //console.log(`intlArray`, intlArray1, `localArray`, localArray1);
+    return {intlArray1, localArray1};
 }
+
 async function getRealData(rawData) {
-    let numbers1=[];
-    let emails1=[];
-    rawData.forEach(e=>{
-        if (e.indexOf(`@`)!==-1) emails1.push(e);
+    let numbers1 = [];
+    let emails1 = [];
+    rawData.forEach(e => {
+        if (e.indexOf(`@`) !== -1) emails1.push(e);
         else numbers1.push(e);
     });
-    return {numbers1,emails1};
+    return {numbers1, emails1};
 };
 module.exports = function (app) {
-    const asyncer=app.get(`wrap`);
-    app.post(`/events/updateContacts`,asyncer( async function (request, response) {
-        let prefix = `+`+request.User.phone.country_prefix;
+    const asyncer = app.get(`wrap`);
+    app.post(`/events/updateContacts`, asyncer(async function (request, response) {
+        let prefix = `+` + request.User.phone.country_prefix;
         var names1 = JSON.parse(request.fields.names);
-        var namesRefined=parseIt(names1,prefix);
+        var namesRefined = parseIt(names1, prefix);
+        console.dir(namesRefined);
         let eventObject = request.app.get(`id`)(request.fields.eventId);
-        let eventEntryBefore = await app.get(`db`)().collection(`events`).findOne({_id:eventObject},{
+        let eventEntryBefore = await app.get(`db`)().collection(`events`).findOne({_id: eventObject}, {
             projection: {
                 unRegisteredNumbersInternational: 1,
                 users: 1,
-                FCM_Tokens:1,
-                childName:1
+                FCM_Tokens: 1,
+                childName: 1
             }
         });
-       //console.log(`DEBUG__`,eventObject,app.get(`db`)().collection(`events`));
-       //console.log(`EVENTS ENTRY BEFORE`,eventEntryBefore);
-       //console.log(`PREFIX`,prefix);
+        //console.log(`DEBUG__`,eventObject,app.get(`db`)().collection(`events`));
+        //console.log(`EVENTS ENTRY BEFORE`,eventEntryBefore);
+        //console.log(`PREFIX`,prefix);
         let rawData = JSON.parse(request.fields.data);
-        let {numbers1,emails1} = await getRealData(rawData);
-        let sms_invite_link=request.app.get(`invite_link`);
+        let {numbers1, emails1} = await getRealData(rawData);
+        let sms_invite_link = request.app.get(`invite_link`);
         //let numberResult = await app.get(`db`)().collection(`events`).find({});
         //let {users, localArray, emails, intlArray} = await createEvent(numbers1, emails1, request.app.get(`db`)(),prefix);
-        let {intlArray1,localArray1} =  await createEvent(numbers1, emails1, request.app.get(`db`)(),prefix);
-        let {users, localArray, emails, intlArray} = await continue_event(numbers1, emails1, request.app.get(`db`)(),prefix,intlArray1,localArray1);
-
+        let {intlArray1, localArray1} = await createEvent(numbers1, emails1, request.app.get(`db`)(), prefix);
+        let {users, localArray, emails, intlArray} = await continue_event(numbers1, emails1, request.app.get(`db`)(), prefix, intlArray1, localArray1);
         let filteredInternational = [];
         let entryPhones = eventEntryBefore.unRegisteredNumbersInternational;
         let newPhones = intlArray;
-        newPhones.forEach(phone=>{
-            if (entryPhones.indexOf(phone)===-1) {
+        newPhones.forEach(phone => {
+            if (entryPhones.indexOf(phone) === -1) {
                 filteredInternational.push(phone);
             }
         });
-       //console.log(`NEW RAW NUMBERS`,filteredInternational,`--++--`,entryPhones,newPhones);
-
-       //console.log(`---`,users,eventEntryBefore.users,`---`);
-        let hashes=[];
-        let newUsers=[];
-        eventEntryBefore.users.forEach(e=>hashes.push(e.toHexString()));
-        users.forEach(e=>{
-            if (hashes.indexOf(e._id.toHexString())===-1) {
+        //console.log(`NEW RAW NUMBERS`,filteredInternational,`--++--`,entryPhones,newPhones);
+        //console.log(`---`,users,eventEntryBefore.users,`---`);
+        let hashes = [];
+        let newUsers = [];
+        eventEntryBefore.users.forEach(e => hashes.push(e.toHexString()));
+        users.forEach(e => {
+            if (hashes.indexOf(e._id.toHexString()) === -1) {
                 newUsers.push(e);
             }
         });
-       //console.log(`NEW USERS NOW ARE`,newUsers);
-        remove(request.email,emails);
+        //console.log(`NEW USERS NOW ARE`,newUsers);
+        remove(request.email, emails);
         //remove(request.User.phone.national_number,localArray);
-        remove(request.User.phone.number,intlArray);
+        remove(request.User.phone.number, intlArray);
         let usersIdsobjs = [];
         newUsers.forEach(e => usersIdsobjs.push(e._id));
 
         let eventsUpdate = await app.get(`db`)().collection(`events`).findOneAndUpdate(
-            {_id:eventObject},
+            {_id: eventObject},
             {
-                $addToSet : {
-                    namesRefined: {$each:namesRefined},
-                    users: {$each:usersIdsobjs} ,
-                    unRegisteredNumbersLocal: {$each:localArray},
-                    unRegisteredNumbersInternational: {$each:filteredInternational},
+                $addToSet: {
+                    namesRefined: {$each: namesRefined},
+                    users: {$each: usersIdsobjs},
+                    unRegisteredNumbersLocal: {$each: localArray},
+                    unRegisteredNumbersInternational: {$each: filteredInternational},
                     unRegisteredEmails: {$each: emails}
                 }
             }
         );
-       //console.log(`UPDATE EVENT CONTACTS DETAIL`,eventsUpdate);
+        //console.log(`UPDATE EVENT CONTACTS DETAIL`,eventsUpdate);
 
         /**
          * Filtering users to respond to only un-responded ones
          */
         let exisitngUsers = await app.get(`db`)().collection(`users`).find({
-            _id: { $in : eventEntryBefore.users }
-        }).project({email:1,FCM_Tokens:1}).toArray();
+            _id: {$in: eventEntryBefore.users}
+        }).project({email: 1, FCM_Tokens: 1}).toArray();
         let allUsers = users.concat(exisitngUsers);
         let allNumbers = eventEntryBefore.unRegisteredNumbersInternational.concat(intlArray);
-        let usersALLNEW = allUsers.map(e=>e.email);
+        let usersALLNEW = allUsers.map(e => e.email);
         let usersUniqueAll = [...new Set(usersALLNEW)];
         let phoneAllUnique = [...new Set(allNumbers)];
-       //console.log(`ALLL`,usersUniqueAll,phoneAllUnique);
+        //console.log(`ALLL`,usersUniqueAll,phoneAllUnique);
         //TODO ADD SUPPORT FOR NON_RESPNDED PHONE NUMEBRS
         let toRespond = await app.get(`db`)().collection(`responses`).find({
-            email: {$in : usersUniqueAll },
-            eventID:eventObject
-        }).project({email:1,FCM_Tokens:1}).toArray();
-        let respndedEmails = toRespond.map(e=>e.email);
-       //console.log(`LOOP START`,respndedEmails,usersUniqueAll);
-        let hjhj=[];
-        usersUniqueAll.forEach(rawEmail=>{
-            if (respndedEmails.indexOf(rawEmail)===-1) {
+            email: {$in: usersUniqueAll},
+            eventID: eventObject
+        }).project({email: 1, FCM_Tokens: 1}).toArray();
+        let respndedEmails = toRespond.map(e => e.email);
+        //console.log(`LOOP START`,respndedEmails,usersUniqueAll);
+        let hjhj = [];
+        usersUniqueAll.forEach(rawEmail => {
+            if (respndedEmails.indexOf(rawEmail) === -1) {
                 hjhj.push(rawEmail);
             }
         });
-       //console.log(`EMAILS`,hjhj,allUsers)
+        //console.log(`EMAILS`,hjhj,allUsers)
         let tokensList = [];
-        hjhj.forEach(email=>{
-            for (let i = 0; i <allUsers.length; i++) {
-                if (allUsers[i].email===email) {
-                    tokensList[email]=allUsers[i].FCM_Tokens;
+        hjhj.forEach(email => {
+            for (let i = 0; i < allUsers.length; i++) {
+                if (allUsers[i].email === email) {
+                    tokensList[email] = allUsers[i].FCM_Tokens;
                     break;
                 }
             }
         });
 
-        let userIdObj = await app.get(`db`)().collection(`users`).findOne({email:request.email},{projection:{name:1}});
-        let named=userIdObj.name;
+        let userIdObj = await app.get(`db`)().collection(`users`).findOne({email: request.email}, {projection: {name: 1}});
+        let named = userIdObj.name;
 
         /*
         Get All unregistered Numbers
          */
 
-        sendPush(tokensList,usersIdsobjs,request.app.get(`db`)(),eventObject,app,named,eventEntryBefore.childName).then(()=>{}).catch(()=>{});
+        sendPush(tokensList, usersIdsobjs, request.app.get(`db`)(), eventObject, app, named, eventEntryBefore.childName).then(() => {
+        }).catch(() => {
+        });
         //sendSMS([...localArray, ...intlArray]);
-        let sendString="";
+        let sendString = "";
         //sendEmails(emails);
-        let send_sms = intlArray.length>0;
-        if (eventsUpdate.ok===1) {
-            response.json({success: true,send_sms,sms_invite_link,send_sms_datas:intlArray.join(";")})
+        let send_sms = intlArray.length > 0;
+        if (eventsUpdate.ok === 1) {
+            response.json({success: true, send_sms, sms_invite_link, send_sms_datas: intlArray.join(";")})
         } else {
             response.json({success: false, message: `Error creating your party`});
         }
