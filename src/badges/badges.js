f14dd96acb944f1cd5c369c3bb97fdb792722972
Abhishek Mathur Windows Machine <mathur17021play@gmail.com>
2019-11-04 12:47:49 +0530

    automated push

--- src/badges/badges.js
+++ src/badges/badges.js
@@ -25,6 +25,12 @@ module.exports=function (app) {
     }));
     app.all(`/badge/invites`,wrapper(async function(request,response,next) {
         let db = app.get(`db`)();
+        let badgesDEBYG = await db.collection(`users`).findOne({email: request.email},{
+            projection: {
+                badgesInvitesGifts:1
+            }
+        });
+        console.log(`DEBUG GIFTS ADD`,badgesDEBYG);
         let badges = await db.collection(`users`).findOne({email: request.email},{
             projection: {
                 badgesInvites:1
@@ -43,7 +49,6 @@ module.exports=function (app) {
     }));
     app.all(`/badge/invitesGifts`,wrapper(async function(request,response,next) {
         console.log(`badges invites triggered`);
-        process.exit(23123);
         let db = app.get(`db`)();
         let badges = await db.collection(`users`).findOne({email: request.email},{
             projection: {
